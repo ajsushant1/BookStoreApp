316d7ec133f870d5d099233fb6e6ed754d864e78
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-05-21 16:52:13 +0530

    ADDED: Method declaration for Custom Query Methods

--- src/main/java/com/bridgelabz/bookstoreapp/book/repository/BookRepository.java
+++ src/main/java/com/bridgelabz/bookstoreapp/book/repository/BookRepository.java
@@ -3,5 +3,10 @@ package com.bridgelabz.bookstoreapp.book.repository;
 import com.bridgelabz.bookstoreapp.book.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findBooksByBookAuthor(String authorName);
+
+    List<Book> findBooksByBookTitle(String bookTitle);
 }
