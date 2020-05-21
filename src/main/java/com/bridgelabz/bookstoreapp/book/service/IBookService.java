d5a99e633162416a58c9d538d858380a9a4a39b0
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-05-21 16:42:49 +0530

    ADDED: Methods to get Book List By Author Name and By Book Title

--- src/main/java/com/bridgelabz/bookstoreapp/book/service/IBookService.java
+++ src/main/java/com/bridgelabz/bookstoreapp/book/service/IBookService.java
@@ -1,9 +1,14 @@
 package com.bridgelabz.bookstoreapp.book.service;
 
+import com.bridgelabz.bookstoreapp.book.exception.BookException;
 import com.bridgelabz.bookstoreapp.book.model.Book;
 
 import java.util.List;
 
 public interface IBookService {
     List<Book> getAllBooks();
+
+    List<Book> getBooksByAuthor(String authorName) throws BookException;
+
+    List<Book> getBooksByTitle(String bookTitle) throws BookException;
 }
