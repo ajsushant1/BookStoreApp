4088cea3081eabe904d042c02f9fc9946d02c785
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-05-21 16:54:24 +0530

    ADDED: Request Method for Finding Book by Author Name and Book Title

--- src/main/java/com/bridgelabz/bookstoreapp/book/controller/BookController.java
+++ src/main/java/com/bridgelabz/bookstoreapp/book/controller/BookController.java
@@ -1,9 +1,11 @@
 package com.bridgelabz.bookstoreapp.book.controller;
 
+import com.bridgelabz.bookstoreapp.book.exception.BookException;
 import com.bridgelabz.bookstoreapp.book.model.Book;
 import com.bridgelabz.bookstoreapp.book.service.IBookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +21,16 @@ public class BookController {
     @GetMapping("/getallbooks")
     public List<Book> getAllBooks() {
         return bookService.getAllBooks();
+    }
+
+    @GetMapping("/getbookbyauthor/{authorName}")
+    public List<Book> getBooksByAuthor(@PathVariable String authorName) throws BookException {
+        return bookService.getBooksByAuthor(authorName);
+    }
+
+    @GetMapping("/getbookbytitle/{bookTitle}")
+    public List<Book> getBooksByTitle(@PathVariable String bookTitle) throws BookException {
+        return bookService.getBooksByTitle(bookTitle);
     }
 
 }
